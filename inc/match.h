baf14c5396a231945d0daa141b384576ca0b58bf
JCly-rikiu <jcly.rikiu@gmail.com>
2020-05-12 08:36:28 +0800

    match images

--- inc/match.h
+++ inc/match.h
@@ -1,11 +1,16 @@
 #ifndef MATCH_
 #define MATCH_
 
+#include <deque>
+#include <tuple>
 #include <vector>
 
 #include "detection.h"
 
 typedef std::vector<std::vector<std::vector<std::tuple<double, double, double, double>>>> MatchPoints;
+typedef std::vector<std::deque<std::tuple<int, double, double>>> PanoramasLists;
+
 MatchPoints match_features(const std::vector<MSOPDescriptor>&);
+PanoramasLists match_images(const MatchPoints&);
 
 #endif
--- src/feature/match.cpp
+++ src/feature/match.cpp
@@ -1,4 +1,8 @@
 #include <algorithm>
+#include <deque>
+#include <iostream>
+#include <random>
+#include <tuple>
 #include <vector>
 
 #include <opencv2/opencv.hpp>
@@ -7,6 +11,8 @@
 #include "match.h"
 
 MatchPoints match_features(const std::vector<MSOPDescriptor>& feature_descriptors) {
+  std::cout << "[Match features...]" << std::endl;
+
   const double ratio_threshold = 0.65;
 
   MatchPoints final_match_points(feature_descriptors.size());
@@ -14,14 +20,14 @@ MatchPoints match_features(const std::vector<MSOPDescriptor>& feature_descriptor
 
   for (size_t image1_i = 0; image1_i != feature_descriptors.size(); image1_i++) {
     for (size_t image2_i = 0; image2_i != image1_i; image2_i++) {
-      auto image1 = feature_descriptors[image1_i];
-      auto image2 = feature_descriptors[image2_i];
+      auto& image1 = feature_descriptors[image1_i];
+      auto& image2 = feature_descriptors[image2_i];
 
       std::vector<std::tuple<double, double, double, double>> match_points1;
       std::vector<std::tuple<double, double, double, double>> match_points2;
       for (size_t layer = 0; layer != image1.size(); layer++) {
-        auto [feature_points1, descriptor1] = image1[layer];
-        auto [feature_points2, descriptor2] = image2[layer];
+        auto& [feature_points1, descriptor1] = image1[layer];
+        auto& [feature_points2, descriptor2] = image2[layer];
 
         std::vector<std::tuple<int, int>> first_matches;
         for (size_t i = 0; i != descriptor1.size(); i++) {
@@ -72,4 +78,164 @@ MatchPoints match_features(const std::vector<MSOPDescriptor>& feature_descriptor
   }
 
   return final_match_points;
+}
+
+std::tuple<int, double, double> translation_RANSAC(
+    const std::vector<std::tuple<double, double, double, double>>& feature_points) {
+  const int k_times = 300;
+  const int n_sample = 3;
+  const double error = 9;
+
+  if (feature_points.size() < n_sample) return {0, 0, 0};
+
+  std::random_device rd;
+  std::mt19937 gen(rd());
+  std::uniform_int_distribution<> dis(0, feature_points.size() - 1);
+
+  double best_ti = 0, best_tj = 0;
+  int max_num_inlier = 0;
+  for (int k = 0; k != k_times; k++) {
+    double ti = 0, tj = 0;
+    for (int n = 0; n != n_sample; n++) {
+      auto [i1, j1, i2, j2] = feature_points[dis(gen)];
+      ti += i1 - i2;
+      tj += j1 - j2;
+    }
+    ti /= n_sample;
+    tj /= n_sample;
+
+    int num_inliers = 0;
+    for (auto [i1, j1, i2, j2] : feature_points) {
+      double ei = i2 + ti - i1;
+      double ej = j2 + tj - j1;
+
+      if (ei * ei + ej * ej < error) num_inliers++;
+    }
+
+    if (num_inliers > max_num_inlier) {
+      max_num_inlier = num_inliers;
+      best_ti = ti;
+      best_tj = tj;
+    }
+  }
+
+  std::cout << "max " << max_num_inlier << " " << feature_points.size() << std::endl;
+
+  if (max_num_inlier < 5.9 + 0.22 * feature_points.size()) return {0, best_ti, best_tj};
+
+  double final_ti = 0, final_tj = 0;
+  for (auto [i1, j1, i2, j2] : feature_points) {
+    double ei = i2 + best_ti - i1;
+    double ej = j2 + best_tj - j1;
+
+    if (ei * ei + ej * ej < error) {
+      final_ti += i1 - i2;
+      final_tj += j1 - j2;
+    }
+  }
+  final_ti /= max_num_inlier;
+  final_tj /= max_num_inlier;
+
+  return {max_num_inlier, final_ti, final_tj};
+}
+
+void search(const int current_image, const bool left_search,
+            const std::vector<std::vector<std::tuple<double, double, int>>>& translations, std::vector<bool>& checked,
+            std::deque<std::tuple<int, double, double>>& list) {
+  for (auto [tj, ti, next_image] : translations[current_image]) {
+    if (checked[next_image]) continue;
+
+    if (left_search && tj < 0) {
+      checked[next_image] = true;
+      list.push_front({next_image, ti, tj});
+      search(next_image, true, translations, checked, list);
+    } else if (!left_search && tj >= 0) {
+      checked[next_image] = true;
+      list.push_back({next_image, ti, tj});
+      search(next_image, false, translations, checked, list);
+    }
+  }
+}
+
+PanoramasLists match_images(const MatchPoints& match_points) {
+  std::cout << "[Match images...]" << std::endl;
+
+  auto num_image = match_points.size();
+
+  std::vector<std::vector<std::tuple<double, double, int>>> left_translations(num_image);
+  std::vector<std::vector<std::tuple<double, double, int>>> right_translations(num_image);
+
+  for (size_t image1_i = 0; image1_i != num_image; image1_i++) {
+    for (size_t image2_i = 0; image2_i != num_image; image2_i++) {
+      if (image1_i == image2_i) continue;
+      auto [max_i, ti, tj] = translation_RANSAC(match_points[image1_i][image2_i]);
+
+      std::cout << "image: " << image1_i << " " << image2_i << std::endl;
+      std::cout << max_i << " " << ti << " " << tj << std::endl;
+
+      if (max_i != 0) {
+        if (tj < 0)
+          left_translations[image1_i].emplace_back(tj, ti, image2_i);
+        else
+          right_translations[image1_i].emplace_back(tj, ti, image2_i);
+      }
+    }
+    std::sort(left_translations[image1_i].begin(), left_translations[image1_i].end(),
+              std::greater<std::tuple<double, double, int>>());
+    std::sort(right_translations[image1_i].begin(), right_translations[image1_i].end());
+  }
+
+  std::vector<bool> checked(num_image, false);
+  PanoramasLists panoramas_lists;
+  for (size_t i = 0; i != num_image; i++) {
+    if (checked[i]) continue;
+    std::deque<std::tuple<int, double, double>> list;
+    list.push_back({i, 0, 0});
+
+    // check the current image index late
+    // if there is a 360 degree panoramas, it will generate a double head deque
+    // the double head can help the drift corrections
+    // notic we must search for right first
+    search(i, false, right_translations, checked, list);
+    search(i, true, left_translations, checked, list);
+    checked[i] = true;
+
+    // since we search for right first, the first head is the original head
+    auto zero_it = std::find_if(list.begin(), list.end(), [&](const auto& e) {
+      auto [image, ti, tj] = e;
+      return image == static_cast<int>(i);
+    });
+
+    // recover the positive translations
+    double ri = 0, rj = 0;
+    for (auto it = zero_it; it < list.end(); it++) {
+      auto& [image, ti, tj] = *it;
+      ri += ti;
+      rj += tj;
+      ti = ri;
+      tj = rj;
+    }
+
+    // recover the negative translations
+    ri = 0, rj = 0;
+    for (auto it = zero_it; it >= list.begin(); it--) {
+      auto& [image, ti, tj] = *it;
+      ri += ti;
+      rj += tj;
+      ti = ri;
+      tj = rj;
+    }
+
+    // recover all translations
+    ri = -ri;
+    rj = -rj;
+    for (auto& [image, ti, tj] : list) {
+      ti += ri;
+      tj += rj;
+    }
+
+    panoramas_lists.push_back(list);
+  }
+
+  return panoramas_lists;
 }
