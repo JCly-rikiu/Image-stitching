33d8db4265bfb86eb282fa41e9ce31d578f9eb6a
JCly-rikiu <jcly.rikiu@gmail.com>
2020-05-13 07:11:27 +0800

    use cv::flann

--- src/feature/match.cpp
+++ src/feature/match.cpp
@@ -13,7 +13,7 @@
 MatchPoints match_features(const std::vector<MSOPDescriptor>& feature_descriptors) {
   std::cout << "[Match features...]" << std::endl;
 
-  const double ratio_threshold = 0.65;
+  const float ratio_threshold = 0.65;
 
   MatchPoints final_match_points(feature_descriptors.size());
   for (auto& v : final_match_points) v.resize(feature_descriptors.size());
@@ -23,49 +23,33 @@ MatchPoints match_features(const std::vector<MSOPDescriptor>& feature_descriptor
       auto& image1 = feature_descriptors[image1_i];
       auto& image2 = feature_descriptors[image2_i];
 
-      std::vector<std::tuple<double, double, double, double>> match_points1;
-      std::vector<std::tuple<double, double, double, double>> match_points2;
+      std::vector<std::tuple<float, float, float, float>> match_points1;
+      std::vector<std::tuple<float, float, float, float>> match_points2;
       for (size_t layer = 0; layer != image1.size(); layer++) {
         auto& [feature_points1, descriptor1] = image1[layer];
         auto& [feature_points2, descriptor2] = image2[layer];
 
-        std::vector<std::tuple<double, int, double>> j_nearest(
-            descriptor2.size(), {std::numeric_limits<double>::max(), -1, std::numeric_limits<double>::max()});
-        std::vector<std::tuple<int, int>> first_matches;
-        for (size_t i = 0; i != descriptor1.size(); i++) {
-          cv::Mat patch_i = descriptor1[i];
+        cv::flann::GenericIndex<cvflann::L2<float>> index1(descriptor1, cvflann::KDTreeIndexParams());
+        cv::flann::GenericIndex<cvflann::L2<float>> index2(descriptor2, cvflann::KDTreeIndexParams());
 
-          double d1 = std::numeric_limits<double>::max(), d2 = std::numeric_limits<double>::max();
-          int j1 = 0;
-          for (size_t j = 0; j != descriptor2.size(); j++) {
-            double norm = cv::norm(patch_i - descriptor2[j]);
-            if (d1 > norm) {
-              d2 = d1;
-              d1 = norm;
-              j1 = j;
-            } else if (d2 > norm) {
-              d2 = norm;
+        cv::Mat_<int> indices2(descriptor1.rows, 2);
+        cv::Mat_<float> distances2(descriptor1.rows, 2);
+        index2.knnSearch(descriptor1, indices2, distances2, 2, cvflann::SearchParams());
+
+        for (int i = 0; i != descriptor1.rows; i++) {
+          auto d2 = distances2.ptr<float>(i);
+          if (d2[0] < ratio_threshold * d2[1]) {
+            int j = indices2.at<int>(i, 0);
+            auto patch2 = descriptor2.row(j);
+
+            std::vector<int> indices1(2);
+            std::vector<float> distances1(2);
+            index1.knnSearch(patch2, indices1, distances1, 2, cvflann::SearchParams());
+
+            if (distances1[0] < ratio_threshold * distances1[1]) {
+              match_points1.emplace_back(std::tuple_cat(feature_points1[i], feature_points2[j]));
+              match_points2.emplace_back(std::tuple_cat(feature_points2[j], feature_points1[i]));
             }
-
-            auto& [jd1, ji1, jd2] = j_nearest[j];
-            if (jd1 > norm) {
-              jd2 = jd1;
-              jd1 = norm;
-              ji1 = i;
-            } else if (jd2 > norm) {
-              jd2 = norm;
-            }
-          }
-
-          if (d1 < ratio_threshold * d2) first_matches.emplace_back(i, j1);
-        }
-
-        for (auto [f1, f2] : first_matches) {
-          auto [jd1, ji1, jd2] = j_nearest[f2];
-
-          if (jd1 < ratio_threshold * jd2 && ji1 == f1) {
-            match_points1.emplace_back(std::tuple_cat(feature_points1[f1], feature_points2[f2]));
-            match_points2.emplace_back(std::tuple_cat(feature_points2[f2], feature_points1[f1]));
           }
         }
       }
@@ -78,11 +62,11 @@ MatchPoints match_features(const std::vector<MSOPDescriptor>& feature_descriptor
   return final_match_points;
 }
 
-std::tuple<int, double, double> translation_RANSAC(
-    const std::vector<std::tuple<double, double, double, double>>& feature_points) {
+std::tuple<int, float, float> translation_RANSAC(
+    const std::vector<std::tuple<float, float, float, float>>& feature_points) {
   const int k_times = 300;
   const int n_sample = 6;
-  const double error = 170;
+  const float error = 170;
 
   if (feature_points.size() < n_sample) return {0, 0, 0};
 
@@ -90,10 +74,10 @@ std::tuple<int, double, double> translation_RANSAC(
   std::mt19937 gen(rd());
   std::uniform_int_distribution<> dis(0, feature_points.size() - 1);
 
-  double best_ti = 0, best_tj = 0;
+  float best_ti = 0, best_tj = 0;
   int max_num_inlier = 0;
   for (int k = 0; k != k_times; k++) {
-    double ti = 0, tj = 0;
+    float ti = 0, tj = 0;
     for (int n = 0; n != n_sample; n++) {
       auto [i1, j1, i2, j2] = feature_points[dis(gen)];
       ti += i1 - i2;
@@ -104,8 +88,8 @@ std::tuple<int, double, double> translation_RANSAC(
 
     int num_inliers = 0;
     for (auto [i1, j1, i2, j2] : feature_points) {
-      double ei = i2 + ti - i1;
-      double ej = j2 + tj - j1;
+      float ei = i2 + ti - i1;
+      float ej = j2 + tj - j1;
 
       if (ei * ei + ej * ej < error) num_inliers++;
     }
@@ -119,10 +103,10 @@ std::tuple<int, double, double> translation_RANSAC(
 
   if (max_num_inlier < 5.9 + 0.22 * feature_points.size()) return {0, best_ti, best_tj};
 
-  double final_ti = 0, final_tj = 0;
+  float final_ti = 0, final_tj = 0;
   for (auto [i1, j1, i2, j2] : feature_points) {
-    double ei = i2 + best_ti - i1;
-    double ej = j2 + best_tj - j1;
+    float ei = i2 + best_ti - i1;
+    float ej = j2 + best_tj - j1;
 
     if (ei * ei + ej * ej < error) {
       final_ti += i1 - i2;
@@ -136,8 +120,8 @@ std::tuple<int, double, double> translation_RANSAC(
 }
 
 void search(const int current_image, const bool left_search,
-            const std::vector<std::vector<std::tuple<double, double, int>>>& translations, std::vector<bool>& checked,
-            std::deque<std::tuple<int, double, double>>& list) {
+            const std::vector<std::vector<std::tuple<float, float, int>>>& translations, std::vector<bool>& checked,
+            std::deque<std::tuple<int, float, float>>& list) {
   for (auto [tj, ti, next_image] : translations[current_image]) {
     if (checked[next_image]) continue;
     checked[next_image] = true;
@@ -154,8 +138,8 @@ PanoramasLists match_images(const MatchPoints& match_points) {
 
   auto num_image = match_points.size();
 
-  std::vector<std::vector<std::tuple<double, double, int>>> left_translations(num_image);
-  std::vector<std::vector<std::tuple<double, double, int>>> right_translations(num_image);
+  std::vector<std::vector<std::tuple<float, float, int>>> left_translations(num_image);
+  std::vector<std::vector<std::tuple<float, float, int>>> right_translations(num_image);
 
   for (size_t image1_i = 0; image1_i != num_image; image1_i++) {
     for (size_t image2_i = 0; image2_i != num_image; image2_i++) {
@@ -164,7 +148,7 @@ PanoramasLists match_images(const MatchPoints& match_points) {
 
       if (max_i != 0) {
         std::cout << "\timage: " << image1_i << " -> " << image2_i << " : " << max_i << " / "
-                  << match_points[image1_i][image2_i].size() << " tj = " << tj << std::endl;
+                  << match_points[image1_i][image2_i].size() << "  ti = " << ti << " tj = " << tj << std::endl;
         if (tj < 0)
           left_translations[image1_i].emplace_back(tj, ti, image2_i);
         else
@@ -172,7 +156,7 @@ PanoramasLists match_images(const MatchPoints& match_points) {
       }
     }
     std::sort(left_translations[image1_i].begin(), left_translations[image1_i].end(),
-              std::greater<std::tuple<double, double, int>>());
+              std::greater<std::tuple<float, float, int>>());
     std::sort(right_translations[image1_i].begin(), right_translations[image1_i].end());
   }
 
@@ -180,7 +164,7 @@ PanoramasLists match_images(const MatchPoints& match_points) {
   PanoramasLists panoramas_lists;
   for (size_t i = 0; i != num_image; i++) {
     if (checked[i]) continue;
-    std::deque<std::tuple<int, double, double>> list;
+    std::deque<std::tuple<int, float, float>> list;
     list.push_back({i, 0, 0});
 
     // check the current image index late
@@ -198,7 +182,7 @@ PanoramasLists match_images(const MatchPoints& match_points) {
     });
 
     // recover the positive translations
-    double ri = 0, rj = 0;
+    float ri = 0, rj = 0;
     for (auto it = zero_it; it < list.end(); it++) {
       auto& [image, ti, tj] = *it;
       ri += ti;
